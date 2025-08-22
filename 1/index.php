5d9bd940929e5e9315c0a50b245d4b960e1ff277
Roberto <robertosarta.rm@gmail.com>
2025-08-22 21:02:58 +0200

    Termino insercion de tcl con rollback mostrando el resultado antes y despues de hacer el rollback

--- 1/index.php
+++ 1/index.php
@@ -25,11 +25,11 @@ include 'conexion.php'; //preguntar que hace esto exactamente
             echo "No hay productos.";
         }
     ?>
-
-    <h2>Numero de pedidos del mes actual</h2>
+    <!-- fin ejercicio -->
+    
+    <h1>Numero de pedidos del mes actual</h1>
     <?php
-    $sql = "SELECT COUNT(*) AS pedidos_mes
-            FROM pedido
+    $sql = "SELECT COUNT(*) AS pedidos_mes FROM pedido
             WHERE MONTH(fecha) = MONTH(CURDATE())
             AND YEAR(fecha) = YEAR(CURDATE())";
     $result = $conn->query($sql);
@@ -40,7 +40,49 @@ include 'conexion.php'; //preguntar que hace esto exactamente
     } else {
         echo"<p>No hay pedidos este mes.</p>";
     }
+    ?>
+    <!-- fin ejercicio -->
+    
+    <h1>2 inserciones TCL con ROLLBACK</h1>
+    <?php
+    $conn->autocommit(FALSE);
+    try {
+        $conn->query("INSERT INTO producto (nombre, precio, stock, id_categoria) VALUES ('Tablet', 250.00, 6, 1)");
+        $conn->query("INSERT INTO producto (nombre, precio, stock, id_categoria) VALUES ('smartwatch', 150.00, 10, 1)");
+
+        echo "<h2>Antes del rollback:</h2>";
+        $result = $conn->query("SELECT * FROM producto");
+        if ($result->num_rows > 0) {
+            echo"<ul>";
+            while ($row = $result->fetch_assoc()) {
+                echo "<li>" . $row['id_producto'] . " - " . $row['nombre'] . " | Precio: " . $row['precio'] . " | Stock: " . $row['stock'] . "</li>";
+            }
+            echo "</ul>";
+        } else {
+            echo"<p>No productos en la tabla.</p>";
+        }
+
+        $conn->rollback();
+
+        echo "<h2>Despues del rollback:</h2>";
+        $sql = "SELECT * FROM producto";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            echo"<ul>";
+            while ($row = $result->fetch_assoc()) {
+                echo "<li>" . $row['id_producto'] . " - " . $row['nombre'] . " | Precio: " . $row['precio'] . " | Stock: " . $row['stock'] . "</li>";
+            }
+            echo "</ul>";
+        } else {
+            echo"<p>No productos en la tabla.</p>";
+        }
+    } catch (Exception $e) {
+        echo "<p>Error: " . $e->getMessage() . "</p>";
+    }
+
     $conn->close();
     ?>
+    
 </body>
 </html>
+
